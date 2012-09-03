ab9b4035267351ddd6bccc6f36cfa01316cc5ffd
moteus <mimir@newmail.ru>
2012-09-03 12:33:50 +0400

    Implement writer.filter

---
+++ lua/log/writer/filter/lvl/le.lua
@@ -0,0 +1,12 @@
+local Log = require "log"
+
+local M = {}
+
+function M.new(max_lvl, writer)
+  assert((max_lvl == 0) or (Log.LVL_NAMES[max_lvl]))
+  return function(msg, lvl, now)
+    if lvl <= max_lvl then writer(msg, lvl, now) end
+  end
+end
+
+return M
