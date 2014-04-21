450fe2179b2bf51cfbafb8d79b96812889933059
Alexey Melnichuk <mimir@newmail.ru>
2014-04-21 15:20:52 +0500

    Update. Use numeric for instead of ipairs

--- lua/log/writer/list.lua
+++ lua/log/writer/list.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.new(...)
   local writers = {...}
   return function(...)
-    for i, writer in ipairs(writers) do
-      writer(...)
+    for i = 1, #writers do
+      writers[i](...)
     end
   end
 end
