ddc2f0737db5c863d9fcc2a37d4b333479ca5f26
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-09-15 16:48:00 +0300

    Back to develop version

--- lua/log.lua
+++ lua/log.lua
@@ -3,7 +3,7 @@
 --
 
 local _COPYRIGHT = "Copyright (C) 2013-2016 Alexey Melnichuk";
-local _VERSION   = "0.1.6"
+local _VERSION   = "0.1.7-dev"
 
 local table  = require "table"
 local string = require "string"
